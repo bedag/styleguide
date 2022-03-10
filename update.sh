8f22e9dc69e4da639031b08d9f6145bafc25d20f
Christian Basler <christian.basler@bedag.ch>
2022-03-10 07:31:46 +0100

    Changes for github.io

--- update.sh
+++ update.sh
@@ -8,3 +8,6 @@ mv ../bedag-angular-styleguide/frontend/dist/app docs
 
 sed -i "s#<base href=\"/\">#<base href=\"/styleguide/\">#" docs/index.html
 
+# Hack to make paths within the app work
+cp docs/index.html docs/404.html
+
